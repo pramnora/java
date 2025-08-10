0a9796de696b34c46256a3bb3208ff7fe121f53e
Paul Ramnora <pramnora@yahoo.com>
2025-08-10 21:54:19 +0100

    Create add01.java
    Created file: add01.java

---
+++ onecompiler/number/add01.java
@@ -0,0 +1,12 @@
+// PROGRAM: Add 2 numeric literals...
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      System.out.println(1+2);                   // outputs: 3
+  }
+}
+
+// This program can be run online inside of your web browser by going to:
+// https://onecompiler.com/java/43tb93yps
