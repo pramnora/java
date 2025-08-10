72c0b39b610ad4d22e8fa7dbb77c86c29779d5b5
Paul Ramnora <pramnora@yahoo.com>
2025-08-10 22:29:26 +0100

    Create add02.java
    Created file: [add02.java] Calling a sub-routine to add 2 passed in numbers.

---
+++ onecompiler/number/add02.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+// Calling a sub-routine...
+// which adds 2 numbers.
+
+public class Main {
+    public static void main(String[] args) {
+    test(9,15);
+  }
+  
+  static void test(int num1,int num2){
+      System.out.println(num1+num2);
+  }
+
+}
+
+// This program can be run online inside of your web browser by going to:  
+// https://onecompiler.com/java/43tbaq7wv  
+
