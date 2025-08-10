11b192df9bb8e5f4090ae2077b00c1f4e1ebf8a5
Paul Ramnora <pramnora@yahoo.com>
2025-08-10 21:13:52 +0100

    Create hw01.java
    Copied and pasted file: [hw01.java] Default 'Hello, world!' program from: onecompiler.com/java

---
+++ onecompiler/hw01.java
@@ -0,0 +1,15 @@
+/* NOTE: This is the 'default' application source code text which is found at...
+          https://www.onecompiler.com/java
+          ...that you can, immediately, run.
+   -----------------------------------------------------------------------------        
+   CREATED: Sun 10 Aug 2025 21:12 PM GMT
+   UPDATED: Sun 10 Aug 2025 21:12 PM GMT
+*/ 
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      System.out.println("Hello, World!");
+  }
+}
